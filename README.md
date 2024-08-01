37a392813329d373cea3a27e20281b00b5330657
Roy Daniel <roydanielr@gmail.com>
2024-08-01 06:36:34 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Frontegg Hosted Login Box Customization Overrides Server
+
+This project is an implementation of the [Frontegg Metadata Overrides](https://docs.frontegg.com/docs/hosted-and-embedded-setup#getting-started-with-metadataoverrides) setup. It provides a Node.js server that handles metadata overrides for customizing the Frontegg login box.
---
+++ server.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 8000;
+
+const corsOptions = {
+  origin: '*', 
+  methods: 'GET,POST,OPTIONS',
+  allowedHeaders: 'Content-Type,Authorization',
+};
+
+app.use(cors(corsOptions)); // Apply CORS middleware globally
+app.use(express.json());
+
+// Middleware to log requests
+app.use((req, res, next) => {
+  console.log('Received request on', req.path);
+  console.log('Request Method:', req.method);
+  console.log('Request Headers:', JSON.stringify(req.headers, null, 2));
+  console.log('Request Body:', JSON.stringify(req.body, null, 2));
+  console.log('Request Query:', JSON.stringify(req.query, null, 2));
+  next();
+});
+
+app.get('/overrides', (req, res) => {
+  res.send({
+    themeV2: {
+      loginBox: {
+        signup: {
+          splitFullName: true,
+        },
+      },
+    },
+    localizations: {
+      en: {
+        loginBox: {
+          signup: {
+            disclaimerCheckboxLabel: 'Acme',
+            termsLinkText: 'Terms & Conditions',
+          },
+        },
+      },
+    },
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
